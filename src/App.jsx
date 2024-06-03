d7b336c27ca8bb4e82a80d00bed11f314b8a1fac
Saumya S kala <162576812+sksk1955@users.noreply.github.com>
2024-06-03 12:50:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
+import Education from "./Components/Education";
 
 import "./styles.css";
 
@@ -24,15 +25,11 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Saumya Kala",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  email: "samarthkala1710@gmail.com",
+  gitHub: "https://github.com/sksk1955",
+  linkedIn: " www.linkedin.com/in/saumya-s-kala-b8027b2bb ",
 };
 
 const primaryColor = "#4E567E";
@@ -44,6 +41,7 @@ const App = () => {
       <Header />
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
+      <Education />
       <Portfolio />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
